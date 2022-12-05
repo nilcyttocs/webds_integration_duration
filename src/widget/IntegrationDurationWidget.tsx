760c545b98ab3f66d04bf7acc339733eff7d35be
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-05 13:03:02 +0800

    Consolidate div elements for widget styling

--- src/widget/IntegrationDurationWidget.tsx
+++ src/widget/IntegrationDurationWidget.tsx
@@ -18,12 +18,8 @@ export class IntegrationDurationWidget extends ReactWidget {
 
   render(): JSX.Element {
     return (
-      <div id={this.id + "_container"} className="jp-webds-widget-container">
-        <div id={this.id + "_content"} className="jp-webds-widget">
-          <IntegrationDurationComponent service={this.service} />
-        </div>
-        <div className="jp-webds-widget-shadow jp-webds-widget-shadow-top"></div>
-        <div className="jp-webds-widget-shadow jp-webds-widget-shadow-bottom"></div>
+      <div id={this.id + "_component"}>
+        <IntegrationDurationComponent service={this.service} />
       </div>
     );
   }
