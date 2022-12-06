a9c321036373d92288f18b84a1ffcd449dcc015f
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-06 11:23:52 +0800

    Remove redundant async

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: "@webds/integration_duration:plugin",
   autoStart: true,
   requires: [ILauncher, ILayoutRestorer, WebDSService],
-  activate: async (
+  activate: (
     app: JupyterFrontEnd,
     launcher: ILauncher,
     restorer: ILayoutRestorer,
