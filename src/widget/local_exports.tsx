72b78ac133e6624cd34eb08754086892927ea893
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-07 22:28:45 -0800

    Set up link to adc_plots

--- src/widget/local_exports.tsx
+++ src/widget/local_exports.tsx
@@ -1,3 +1,9 @@
+import React from "react";
+
+import { TouchcommADCReport } from "@webds/service";
+
 export { ContextData, Context } from "./IntegrationDurationComponent";
 
 export { requestAPI } from "../handler";
+
+export const ADCDataContext = React.createContext([] as TouchcommADCReport[]);
