c54b1a966725f8ad494d87e9ac19320317cef0be
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-26 12:52:52 +0800

    Update local exports

--- src/local_exports.ts
+++ src/local_exports.ts
@@ -1,11 +1,3 @@
-import React from "react";
-
-import { TouchcommADCReport } from "@webds/service";
-
 export { webdsService } from "./widget/IntegrationDurationWidget";
 
-export { ContextData, Context } from "./widget/IntegrationDurationComponent";
-
 export { requestAPI } from "./handler";
-
-export const ADCDataContext = React.createContext([] as TouchcommADCReport[]);
--- src/widget/local_exports.tsx
+++ src/widget/local_exports.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import { TouchcommADCReport } from "@webds/service";
 
-export { webdsService } from "../local_exports";
+export { ContextData, Context } from "./IntegrationDurationComponent";
 
-export { ContextData, Context } from "../local_exports";
+export { webdsService } from "../local_exports";
 
 export { requestAPI } from "../local_exports";
 
